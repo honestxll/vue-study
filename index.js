0a7439b8e2231c80e5ae189715fa3f05878b5f8e
13451883267 <1131328108@qq.com>
2018-03-05 09:12:47 +0800

    动画

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 Vue.component('family', {
   props: ['person'],
-  template: '<div class="ui item">{{ person.name }} {{ person.age + "岁" }}</div>'
+  template: '<p>{{ person.name }} {{ person.age + "岁" }}</p>'
 })
 var app = new Vue({
   el: '.ui.container',
   data: {
-    tag: false,
+    tag: true,
     family: [
       { name: '陈实', age: 24 },
       { name: '徐露露', age: 25 },
       { name: ' 小陈实', age: 0.7 },
     ],
   },
+  methods: {
+    add() {
+      this.family.push({
+        name: 'test',
+        age: 0
+      })
+    },
+    rm() {
+      this.family.pop();
+    }
+  }
 })
